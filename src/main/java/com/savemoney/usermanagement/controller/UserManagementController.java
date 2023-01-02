878db158c266e143e38869a678c9b947e8018eea
Andrea Arruzza <andrearru@hotmail.it>
2023-01-02 18:51:52 +0100

    add plugin open api
    add spring-boot-maven-plugin
    
    add plugin maven-compiler-plugin con mapstruct e lombok
    
    create api specification user-management.yml

--- src/main/java/com/savemoney/usermanagement/controller/UserManagementController.java
+++ src/main/java/com/savemoney/usermanagement/controller/UserManagementController.java
@@ -1,4 +1,13 @@
 package com.savemoney.usermanagement.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserManagementController {
+
+    @GetMapping(value="/hello")
+    public String hello(){
+        return "Hello!";
+    }
 }
