73501a4080fe780bd5d1715bea0ce7dc6d5e2b06
Andrea Arruzza <andrearru@hotmail.it>
2023-03-07 08:32:18 +0100

    add library edit base packages add microservicecode properties

--- src/main/java/com/savemoney/usermanagement/Application.java
+++ src/main/java/com/savemoney/usermanagement/Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.savemoney")
-@EntityScan(basePackages = "com.savemoney")
+@SpringBootApplication(scanBasePackages = {"com.savemoney", "com.global"})
+@EnableJpaRepositories(basePackages = {"com.savemoney",  "com.global"})
+@EntityScan(basePackages = {"com.savemoney",  "com.global"})
 public class Application {
 
 	public static void main(String[] args) {
